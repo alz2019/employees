ac09af03ab6dba2f2e055fe6a5b94632255509b8
alz2019 <az2019git@rambler.ru>
2021-03-20 04:57:22 +0300

    Remove Repository annotation

--- employees-api/src/main/java/com/alz2019/employeesapi/repository/EmployeeRepository.java
+++ employees-api/src/main/java/com/alz2019/employeesapi/repository/EmployeeRepository.java
@@ -4,6 +4,5 @@ import com.alz2019.employeesapi.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
